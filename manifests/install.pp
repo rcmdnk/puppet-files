9079e17659797789179703cf0e52188e6a92f241
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-07-18 22:36:33 +0900

    fixed install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,30 +1,39 @@
 class files::install(
   $files = {}){
 
-  $files.each |file, opts| {
-    path = opts["path"]
-    source = opts["source"]
-    owner = opts.has_key?("owner") ? opts["owner"] : "root"
-    group = opts.has_key?("group") ? opts["group"] : "root"
-    mode = opts.has_key?("mode") ? opts["mode"] : "0644"
-    file {file:
-      path => path,
-      source => source,
-      owner => owner,
-      group => group,
-      mode => mode,
+  $files.each |String $file, Hash $opts| {
+    $path = $opts["path"]
+    $source = $opts["source"]
+    if has_key($opts, "owner") {
+      $owner = $opts["owner"]
+    }else{
+      $owner = "root"
     }
-    if opts.has_key?("exec") and opts["exec"] {
-      command = path
-      if opts.has_key?("args") {
-        command = path + opts["args"]
+    if has_key($opts, "group") {
+      $group = $opts["group"]
+    }else{
+      $group = "root"
+    }
+    if has_key($opts, "mode") {
+      $mode = $opts["mode"]
+    }else{
+      $mode = "0644"
+    }
+    file {$file:
+      path => $path,
+      source => $source,
+      owner => $owner,
+      group => $group,
+      mode => $mode,
+    }
+    if has_key($opts, "exec") and $opts["exec"] {
+      $command = $path
+      if has_key($opts, "args") {
+        $command = $path + $opts["args"]
       }
-      exec {file:
-        command => command
+      exec {"${file} exec":
+        command => $command
       }
     }
-  }
-  files {$packages:
-    ensure => installed,
   }
 }
