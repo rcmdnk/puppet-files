d33153c054aaabed90b7c4d4a300d2ade06d427c
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-04-03 17:23:06 +0900

    add support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,27 @@
 class files(
   $directories = {},
+  $directories_add = {},
   $files = {},
+  $files_add = {},
   $links = {},
+  $links_add = {},
   $lines = {},
+  $lines_add = {},
   ){
+  $directories_all = deep_merge($directories, $directories_add)
   class { 'files::directories':
-    directories => $directories,
+    directories => $directories_add,
   }
+  $files_all = deep_merge($files, $files_add)
   class { 'files::files':
-    files => $files,
+    files => $files_all,
   }
+  $links_all = deep_merge($links, $links_add)
   class { 'files::links':
-    links => $links,
+    links => $links_all,
   }
+  $lines_all = deep_merge($lines, $lines_add)
   class { 'files::lines':
-    lines => $lines,
+    lines => $lines_all,
   }
 }
